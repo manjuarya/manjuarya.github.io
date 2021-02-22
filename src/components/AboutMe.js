48fb78ed851dfa485fdc880c17bbf37a76c48d77
manju <mann.arya@gmail.com>
2021-02-22 22:35:38 +0100

    newUpdates

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,16 +12,24 @@ export default class AboutMe1 extends Component {
                     <Cell col={6}>
                         <h1>ABOUT ME</h1>
                         <hr/>
-                        <p>Manju Arya is a highly motivated Full-stack developer looking for the opportunity to prove herself in the IT industry. She finds creativity, critical thinking and honesty to be the most valuable qualities for success in a software development career. She has a total of 12 years experience in the telecom domain operating different PSTN switches where she also worked as unit test writer using JUnit framework in a Java project for one year. She has also attended a 16 weeks intensive course in software development from KTH provided by Novare Potential consultancy which got completed in May 2020.</p> 
-                        <p>Manju holds a Bachelor of Technology in Electronics and communication engineering, where she was the secretary of the Fine Arts and hobby counsel. Manju has a strong sense of ownership and value creation and hopes to continue using her determination and critical thinking to be successful and highly sought after full stack developer.</p> 
+                        <p>Manju Arya is a highly motivated Full-stack developer looking for the opportunity to prove herself in the IT industry. 
+                            Currently, she is working as an Frontend developer intern using React JS, HTML, CSS, Google analytics and Swagger APIs. 
+                            She has more than one year experience in React JS, Javascript, CSS and created many mini-projects and 
+                            close to three-year experience in back-end Java development and writing unit tests using Junit framework in a telecom project. 
+                            She has more than 12 years of total experience working as Telecom and software Engineer. 
+                            She finds creativity, critical thinking and honesty to be the most valuable qualities for success in a software development career. </p> 
+                        <p>Manju has Completed KTH’s Software Development Academy-7 intensive 16 weeks software development course in May’2020. 
+                            Improved her skills in ReactJS, HTML, CSS, Google Analytics Java, Spring Boot Framework, MySQL, Docker, Git & Maven. 
+                            She holds a Bachelor of Technology in Electronics and communication engineering, where she was the secretary of the Fine Arts and hobby counsel. 
+                            She has a strong sense of ownership and value creation and hopes to continue using her determination and critical thinking to be successful 
+                            and highly sought after full stack developer.</p> 
                         <p>To hire Manju as an Employee or freelance full stack developer, email her at <em>mann.arya@gmail.com.</em></p>
-
                         <h1>MY VIDEO</h1>
                         <hr/>
                         <div className='player-wrapper'>
                             <ReactPlayer
                             className='react-player'
-                            url='https://youtu.be/BhcvpGgSXc4'
+                            url='https://youtu.be/Ir1SbbnCKYU'
                             width='100%'
                             height='100%'
                             />
